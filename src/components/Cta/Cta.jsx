be46f509fc780ce8166b3657fcb738fb2d3f9d9c
treee97 <116231527+treee97@users.noreply.github.com>
2023-03-22 20:29:24 +0100

    working on CTA and header still

--- src/components/Cta/Cta.jsx
+++ src/components/Cta/Cta.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import {FiArrowRight} from 'react-icons/fi';
 import './Cta.css';
 const CTA = () => (
-    <div>CTA</div>
+    <div className='app__cta'>
+        <p className="app__cta-info p__raleway">Visit the official Wizards of the Coast website!</p>
+        <button type='button'> <FiArrowRight/> Go</button>
+    </div>
 )
 
 export default CTA
--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -9,12 +9,12 @@ const Header = () => (
       <div className="app__header-heading">
         <h1 className="header__cormorant gradient__text">Lorem ipsum dolor sit, amet consect</h1>
         {/* The Epic Adventure You've Been Looking For */}
-        <p className="p__raleway">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos officia repellat illo? Laudantium nesciunt earum obcaecati officiis culpa sunt quos minus, necessitatibus ullam iusto error doloribus, iure quia tempora accusantium.</p>
+        <p className="p__raleway gradient__text">Lorem ipsum dolor sit amet con  minus, necessitatibus ullam iusto error doloribus, iure quia tempora accusantium.</p>
         {/* Experience unique settings made by players like you. An empire of evil demons, elfs fighting in a deadly forest, stealing the most protected bank in the kingdom. Everything can be possible. */}
-        <div className="app__header-heading_button">
+      </div>
+      <div className="app__header-heading_button">
           <Cta />
         </div>
-      </div>
     </div>
 )
 export default Header;
