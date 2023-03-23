395e43bd439b4678edd13e99d8e0821f959a0daf
treee97 <116231527+treee97@users.noreply.github.com>
2023-03-23 20:58:55 +0100

    header and CTA done. To fix media queries

--- src/components/Cta/Cta.jsx
+++ src/components/Cta/Cta.jsx
@@ -3,8 +3,8 @@ import {FiArrowRight} from 'react-icons/fi';
 import './Cta.css';
 const CTA = () => (
     <div className='app__cta'>
-        <p className="app__cta-info p__raleway">Visit the official Wizards of the Coast website!</p>
-        <button type='button'> <FiArrowRight/> Go</button>
+            <p className="app__cta-info p__raleway">Wizards of the Coast</p>
+            <button type='button'> <FiArrowRight/> Go</button>
     </div>
 )
 
--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -7,14 +7,14 @@ const Header = () => (
     <div className='app__header section__padding'>
       {/* <img src={images.topRightBg} alt="" /> */}
       <div className="app__header-heading">
-        <h1 className="header__cormorant gradient__text">Lorem ipsum dolor sit, amet consect</h1>
+        <h1 className="header__cormorant gradient__text">The Adventure Begins Now</h1>
         {/* The Epic Adventure You've Been Looking For */}
-        <p className="p__raleway gradient__text">Lorem ipsum dolor sit amet con  minus, necessitatibus ullam iusto error doloribus, iure quia tempora accusantium.</p>
+        <p className="p__raleway gradient__text">Experience unique settings made by players like you where the only limit is your imagination</p>
         {/* Experience unique settings made by players like you. An empire of evil demons, elfs fighting in a deadly forest, stealing the most protected bank in the kingdom. Everything can be possible. */}
       </div>
       <div className="app__header-heading_button">
           <Cta />
-        </div>
+      </div>
     </div>
 )
 export default Header;
